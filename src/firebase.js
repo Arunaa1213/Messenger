663e77ba075f7347cbf3458f207eca062b316fb3
Arunaa S <arunaasureshkumar@gmail.com>
2023-02-14 10:10:54 +0530

    updated final

--- src/firebase.js
+++ src/firebase.js
@@ -9,14 +9,23 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAjhFpORrpzlXRMg-g7xV1l1jG9iU2RBlI",
-  authDomain: "react-messenger-62fd1.firebaseapp.com",
-  databaseURL: "react-messenger-62fd1.firebaseapp.com",
-  projectId: "react-messenger-62fd1",
-  storageBucket: "react-messenger-62fd1.appspot.com",
-  messagingSenderId: "668444933581",
-  appId: "1:668444933581:web:8842e715ceb5e62c0a9884",
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_DATABASE_URL}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAjhFpORrpzlXRMg-g7xV1l1jG9iU2RBlI",
+//   authDomain: "react-messenger-62fd1.firebaseapp.com",
+//   databaseURL: "react-messenger-62fd1.firebaseapp.com",
+//   projectId: "react-messenger-62fd1",
+//   storageBucket: "react-messenger-62fd1.appspot.com",
+//   messagingSenderId: "668444933581",
+//   appId: "1:668444933581:web:8842e715ceb5e62c0a9884",
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
